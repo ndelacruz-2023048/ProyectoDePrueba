eae6390ef8e2428788690d114dd7609f4fa8787f
ndelacruz-2023048 <ndelacruz-2023048@kinal.edu.gt>
2025-06-06 23:56:05 -0600

    fix: Se quitan textos incorrectos

--- src/App.jsx
+++ src/App.jsx
@@ -32,19 +32,8 @@ function App() {
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
-          Edit <code>src/App.jsx</code> and save to test HMR
-          Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
