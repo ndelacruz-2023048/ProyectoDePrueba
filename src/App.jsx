7cd2c8a89eef40eabccc777257ff3e85dcc1678f
ndelacruz-2023048 <ndelacruz-2023048@kinal.edu.gt>
2025-06-06 23:16:49 -0600

    feat: se crea el formulario de inicio de sesion

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Hola Vite Project</h1>
+      <h1>Hola Vite Project {count} +{count} +{count}= {count*3}</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
