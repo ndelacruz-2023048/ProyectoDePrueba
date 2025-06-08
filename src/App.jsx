e4f940f20caab8ba4536e95a3389d44c00c9f88f
ndelacruz-2023048 <ndelacruz-2023048@kinal.edu.gt>
2025-06-08 01:51:48 -0600

    feat: se agrega mas titulos

--- src/App.jsx
+++ src/App.jsx
@@ -37,6 +37,9 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
   )
 }
